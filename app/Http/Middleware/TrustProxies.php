3f506811e28dc66213c7243f949fef087b08f64f
VictorAlagwu <victoralagwu@gmail.com>
2018-08-16 20:45:35 +0100

    Trusted Proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,18 +12,6 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
-
-    /**
-     * The current proxy header mappings.
-     *
-     * @var array
-     */
-    protected $headers = [
-        Request::HEADER_FORWARDED => 'FORWARDED',
-        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
-        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
-        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
-        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
-    ];
+    protected $proxies = '*';
+    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
 }
